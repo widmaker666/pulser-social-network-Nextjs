9fc408b1e3031b20339c2bcf1795b3e7f234a54b
widmaker666 <widmaker666@msn.com>
2023-08-25 11:46:35 +0200

    style maj

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,6 +1,7 @@
-import styles from "./navbar.module.css";
+import styles from "./Navbar.module.css";
 import Image from "next/image";
-import logoNav from "../assets/images/logoNav.png";
+import logoNav from "../assets/images/logoNavbar.png";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
@@ -8,6 +9,11 @@ const Navbar = () => {
       <nav className={styles.navbar}>
         <div className="logo">
           <Image src={logoNav} alt="" className={styles.imgLogo} />
+        </div>
+        <div className={styles.connectionLinks}>
+          <Link className={styles.register} href="/register">register</Link>
+          <h3>/</h3>
+          <Link className={styles.login} href="/login">login</Link>
         </div>
       </nav>
     </>
--- app/page.js
+++ app/page.js
@@ -1,7 +1,25 @@
+import styles from "./page.module.css";
+
+
 export default function Home() {
   return (
- <>
- <h1>Hello World!</h1>
- </>
-  )
+    <main>
+      <div className={styles["add-post"]}>
+        <form className={styles["form-container"]}>
+          <label htmlFor="title">
+            Title
+          </label>
+          <input required type="text" id="title" placeholder="Your title" />
+          <label htmlFor="description">Description</label>
+          <textarea
+            name="description"
+            id="description"
+            required
+            placeholder="Give us your question"
+          ></textarea>
+          <button type="submit">Pulse it !</button>
+        </form>
+      </div>
+    </main>
+  );
 }
