81b6590fb1d462e5415350172a19ef71342202f1
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-08-25 19:41:50 +0200

    essaie avec adress netlify

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import AddPost from "./components/AddPost";
+import CardPosts from "./components/CardPosts";
+
+export default function Home() {
+  return (
+    <>
+      <AddPost />
+      <section className="card-container">
+        <CardPosts />
+      </section>
+    </>
+  );
+}
