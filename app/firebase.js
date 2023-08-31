95825049155af0bce9b9077d9ce4711d8c6493ae
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-08-31 08:58:29 +0200

    variable environement caché

--- app/firebase.js
+++ app/firebase.js
@@ -5,13 +5,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCUEd0oec7xF6t-IlRtRuB_vXye34P9Dl0",
-  authDomain: "react-firebase-auth-4ddbe.firebaseapp.com",
-  projectId: "react-firebase-auth-4ddbe",
-  storageBucket: "react-firebase-auth-4ddbe.appspot.com",
-  messagingSenderId: "86960069120",
-  appId: "1:86960069120:web:6cc82953ec23dba565d6e5",
+export const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
