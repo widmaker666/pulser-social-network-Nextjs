19ab694260955f03d82c20c1b7d833b3fdc78119
widmaker666 <alexandre.hontcharouk69@gmail.com>
2023-09-04 16:41:02 +0200

    majuser auth validate

--- app/components/AddComments.jsx
+++ app/components/AddComments.jsx
@@ -11,61 +11,70 @@ export default function AddComments({ idComment }) {
 
   const { user } = UserAuth();
   const userId = user && user.uid;
-  const pictureCommentUrl = user && user.photoURL; 
-  
+  const pictureCommentUrl = user && user.photoURL;
 
   const handleComment = async (e) => {
     e.preventDefault();
-    
-      try {
-        const res = await fetch("/api/comments", {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-          },
-          body: JSON.stringify({ pictureCommentUrl, comment, idComment, userId }),
-        });
-  
-        if (res.ok) {
-          alert("comment created");
-          setComment("");
-          router.refresh();
-        } else {
-          throw new Error("failed to fetch comment");
-        }
-      } catch (error) {
-        console.log(error);
+
+    try {
+      const res = await fetch("/api/comments", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify({ pictureCommentUrl, comment, idComment, userId }),
+      });
+
+      if (res.ok) {
+        alert("comment created");
+        setComment("");
+        router.refresh();
+      } else {
+        throw new Error("failed to fetch comment");
       }
-      return;
-    }; 
+    } catch (error) {
+      console.log(error);
+    }
+    return;
+  };
 
   return (
     <>
       <div className={styles["add-comment"]}>
-        <img
-          src={
-            pictureCommentUrl
-              ? pictureCommentUrl
-              : "https://www.gala.fr/imgre/fit/~1~gal~2022~11~03~3a8f53dd-e7b8-4033-9096-5b6e465cd886.jpeg/3578x3226/quality/80/georges-brassens.jpeg"
-          }
-          className={styles.imgCard}
-          width={29}
-          height={29}
-          alt="avatar"
-        />
+        {user ? (
+          <>
+            <img
+              src={
+                pictureCommentUrl
+                  ? pictureCommentUrl
+                  : "https://www.gala.fr/imgre/fit/~1~gal~2022~11~03~3a8f53dd-e7b8-4033-9096-5b6e465cd886.jpeg/3578x3226/quality/80/georges-brassens.jpeg"
+              }
+              className={styles.imgCard}
+              width={29}
+              height={29}
+              alt="avatar"
+            />
 
-        <form onSubmit={handleComment}>
-          <input
-            value={comment}
-            onChange={(e) => setComment(e.target.value)}
-            max="300"
-            type="text"
-            name="comment"
-            id="comment"
-            placeholder="Donne ton opinion..."
-          />
-          <button className="btnComments" type="submit">pulse</button>
-        </form>
+            <form onSubmit={handleComment}>
+              <input
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                max="300"
+                type="text"
+                name="comment"
+                id="comment"
+                placeholder="Donne ton opinion..."
+              />
+              <button className="btnComments" type="submit">
+                pulse
+              </button>
+            </form>
+          </>
+        ) : (
+          <>
+            <h1>Connecte toi pour participer</h1>
+          </>
+        )}
       </div>
     </>
   );
